db43d256286239e96d38ae52bc2dad2642b9827f
Luis Fernando López Hernández <luis.lopez47@gmail.com>
2025-08-27 15:26:23 -0600

    Crear __init__.py para módulo parsers

---
+++ src/parsers/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Módulo de parsers especializados para diferentes fuentes de licitaciones
+"""
+
+from .dof_parser import DOFParser
+
+__all__ = ['DOFParser']
